459301bfb72303eeef7ad99b7e76e3a20d0de67f
Anand <jeevaanand93002@gmail.com>
2026-06-24 12:47:55 +0400

    Model had been updated, used XGBClassifier

--- src/predict.py
+++ src/predict.py
@@ -1,33 +1,51 @@
 
 import pickle
-import xgboost as xgb
-
-from src.preprocess import clean_tweet
-from src.preprocess import tokenize_tweets
-from src.preprocess import vectorize_tweet
+import pandas as pd
+import numpy as np
 
 
+from src.preprocess import clean_text
 
-model = pickle.load(
-    open("./models/sentiment_model.pkl", "rb")
+
+ml_model = pickle.load(
+    open("./models/XGBClassifier-model.pkl", "rb")
+)
+
+vector_model = pickle.load(
+    open("./models/word2vec-model.pkl", "rb")
 )
 
 
+def word_vector(token, size):
+  vec = np.zeros(size).reshape((1, size))
+  count = 0
+  for x in token:
+    try:
+      vec += vector_model.wv[x].reshape((1, size))
+      count += 1
+    except KeyError:
+      pass
+
+  if count != 0:
+    vec /= count
+
+  return vec
+
 def predict_sentiment(text):
 
-    text = clean_tweet(text)
-    tokenize_tweet = tokenize_tweets(text)     
-    word_vec_df = vectorize_tweet(tokenize_tweet)
-
+    text = clean_text(text)
     
+    word_vector_arr = np.zeros((len(text), 200))
+    
+    word_vector_arr = word_vector(text, 200)
 
-    dmatrix = xgb.DMatrix(word_vec_df)
-
-    probability = float(model.predict(dmatrix)[0])
-
-    print(model.predict(dmatrix))
-          
-    prediction=1 if probability >= 0.5 else 0
-
+    word_vec_df = pd.DataFrame(word_vector_arr)
+   
+    probability = ml_model.predict_proba(word_vec_df)
+    
+    prediction = (probability[:, 1] >= 0.3).astype(int)
+    
+    print(prediction, probability)
+    
 
     return prediction, probability
