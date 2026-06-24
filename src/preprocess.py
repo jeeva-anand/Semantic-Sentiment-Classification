3398901146cc84e3ed8788555b1b8d3382b32d84
Anand <86646740+jeeva-anand@users.noreply.github.com>
2026-06-24 14:35:25 +0400

    Enhance text cleaning with stemming and stop words
    Updated the text cleaning process to include stemming and stop word removal.

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,10 @@
 import re
-
+import nltk
+from nltk import PorterStemmer
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+ps = PorterStemmer()
 
 def clean_text(text):
 
@@ -8,7 +13,10 @@ def clean_text(text):
     text = re.sub(r"#\w+", "", text)
     text = re.sub(r"[^a-zA-Z]", " ", text)
 
-    words = text.split()
-    words = [w for w in words if len(w) > 2]
+    words = text.split()    
+    words = [w for w in words if len(w) > 3]
+    words = [ps.stem(w) for w in words if w not in stop_words]    
+    
 
-    return " ".join(words)
+    return words
+
